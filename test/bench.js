7f9fcbf06f179ee9982a4c0ede45cc509b742c67
Daniel <danieliozzi@gmail.com>
2020-01-31 10:41:50 +0200

    bench & flame

--- test/bench.js
+++ test/bench.js
@@ -2,27 +2,134 @@ const { default: createStore } = require("../dist");
 const levelup = require("levelup");
 const MemDown = require("memdown");
 const subleveldown = require("subleveldown");
+const assert = require("assert");
 
-const MemDb = () => levelup(new MemDown());
+const randomString = (length = 16, enc = "hex") =>
+  require("crypto")
+    .randomBytes(length)
+    .toString(enc);
 
-function randomString() {
-  return require("crypto")
-    .randomBytes(16)
-    .toString("hex");
-}
-async function run() {
-  const store = createStore(MemDb());
-  await store.add({ id: "1", name: "1" });
+const noop = () => {};
+const silent = process.argv.indexOf("--silent") !== -1;
+const time = silent ? noop : console.time.bind(console);
+const timeEnd = silent ? noop : console.timeEnd.bind(console);
+const log = silent ? noop : console.log.bind(console);
+
+async function run(level) {
   const loopCount = 10000;
-  for (let i = 0; i < loopCount; i++) {
-    await store.add({ id: `indexed${i}`, name: `x${i}` });
+
+  const store = createStore(level, "id");
+  {
+    await (() => {
+      time("add:1");
+      return store
+        .add({ id: "1", name: "1" }) //
+        .then(timeEnd("add:1"));
+    })();
   }
-  const one = 9999;
-  await store.findOne(`indexed${one}`);
-  await store.findMany();
+  {
+    time("remove:one");
+    await store.remove("1");
+    timeEnd("remove:one");
+  }
+  {
+    time(`add:x${loopCount}`);
+    for (let i = 0; i < loopCount; i++) {
+      await store.add({ id: `indexed${i}`, name: `x${i}` });
+    }
+    timeEnd(`add:x${loopCount}`);
+  }
+  {
+    time(`remove:${loopCount}`);
+    await store.remove("*");
+    timeEnd(`remove:${loopCount}`);
+  }
+  {
+    time(`add:x${loopCount}`);
+    for (let i = 0; i < loopCount; i++) {
+      await store.add({ id: `indexed${i}`, name: `x${i}` });
+    }
+    timeEnd(`add:x${loopCount}`);
+  }
+  {
+    const one = 9999;
+    time(`findOne:${one}`);
+    const x = await store.findOne(`indexed${one}`).then(x => {
+      timeEnd(`findOne:${one}`);
+      return x;
+    });
+    assert.equal(x.name, `x${one}`);
+  }
+  {
+    time(`update:x${loopCount}`);
+    for (let i = 0; i < loopCount; i++) {
+      await store.update({ id: `indexed${i}`, name: `x${i}!` });
+    }
+    timeEnd(`update:x${loopCount}`);
+  }
+  {
+    const one = 9999;
+    time(`findOne:${one}`);
+    const x = await store.findOne(`indexed${one}`).then(x => {
+      timeEnd(`findOne:${one}`);
+      return x;
+    });
+    assert.equal(x.name, `x${one}!`);
+  }
+  {
+    time("sub-level:stream");
+    const ret = await new Promise((resolve, reject) => {
+      const stream = level.createReadStream();
+      const result = [];
+      stream.on("data", data => {
+        result.push(data);
+      });
+      stream.on("end", () => {
+        resolve(result);
+      });
+      stream.on("error", error => {
+        reject(error);
+      });
+    });
+    timeEnd("sub-level:stream");
+    assert.equal(ret.length, loopCount);
+  }
+  {
+    time("sub-level:stream");
+    const ret = await new Promise((resolve, reject) => {
+      const stream = level.createReadStream();
+      const result = [];
+      stream.on("data", data => {
+        result.push(data);
+      });
+      stream.on("end", () => {
+        resolve(result);
+      });
+      stream.on("error", error => {
+        reject(error);
+      });
+    });
+    timeEnd("sub-level:stream");
+    assert.equal(ret.length, loopCount);
+  }
+  {
+    time(`findMany:${loopCount}`);
+    const x = await store.findMany();
+    timeEnd(`findMany:${loopCount}`);
+    assert.equal(x.length, loopCount);
+  }
+  {
+    time("findMany:query");
+    const found = await store.findMany({
+      name: { $in: ["x9999!"] },
+    });
+    timeEnd("findMany:query");
+    assert.equal(found[0].name, "x9999!");
+  }
 }
-run();
-run(subleveldown(
-  MemDb(), 
-  randomString(), 
-  { valueEncoding: "json" }));  
+const db = levelup(new MemDown());
+log("...");
+time("run");
+run(subleveldown(db, randomString(), { valueEncoding: "json" })).then(() =>
+  timeEnd("run"),
+);
