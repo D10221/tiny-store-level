0b8f5b06e18555ff718175abbf23a8073e8b40fe
Daniel <danieliozzi@gmail.com>
2020-02-01 10:12:50 +0200

    can test other downs

--- test/db.js
+++ test/db.js
@@ -1,5 +1,13 @@
 const levelup = require("levelup");
-const MemDown = require("memdown");
+const memdown = require("memdown");
+const leveldown = require("leveldown");
+const path = require("path");
 /** @type {import("levelup").LevelUp} */
-const db = levelup(new MemDown());
-module.exports = db;
+module.exports = (() => {
+  if (process.env.LEVELDOWN === "leveldown") {
+    console.log("DOWN: leveldown")
+    return levelup(leveldown(path.join(__dirname, "../testdb")));
+  }
+  console.log("DOWN: memdown")
+  return levelup(memdown());
+})();
